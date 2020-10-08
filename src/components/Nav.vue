578f73b1893451ee65c4be9c25898f84c1c7b713
bbter <1035675608@qq.com>
2020-10-08 19:47:23 +0800

    创建Icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="nav">
-    <RouterLink to="/money" >记账</RouterLink>
+    <RouterLink to="/money">
+      <Icon name="money"/>
+      记账
+    </RouterLink>
     |
-    <RouterLink to="/labels" >标签</RouterLink>
+    <RouterLink to="/labels">
+      <Icon name="labels"/>
+      标签
+    </RouterLink>
     |
-    <RouterLink to="/statistics">统计</RouterLink>
+    <RouterLink to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </RouterLink>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "Nav"
-}
+  name: 'Nav'
+};
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
-  }
+.nav {
+  border: 1px solid red;
+}
 </style>
