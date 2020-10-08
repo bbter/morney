d92c2fd5d19bcc5081cb96c801c377bf6400ecd1
bbter <1035675608@qq.com>
2020-10-08 16:17:27 +0800

    全局组件<Nav/>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <router-view />
-    <hr>
-    <div>
-      <RouterLink to="/money" >记账</RouterLink>
-      |
-      <RouterLink to="/labels" >标签</RouterLink>
-      |
-      <RouterLink to="/statistics">统计</RouterLink>
-    </div>
   </div>
 </template>
 
---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <RouterLink to="/money" >记账</RouterLink>
+    |
+    <RouterLink to="/labels" >标签</RouterLink>
+    |
+    <RouterLink to="/statistics">统计</RouterLink>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "Nav"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
