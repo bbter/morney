5928d8e2622ac2b4b80c63bf32beeadf7ad55dfb
bbter <1035675608@qq.com>
2020-10-09 11:48:29 +0800

    添加全局样式和全局变量 更改money icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable */
 const path = require('path')
 module.exports = {
   lintOnSave: false,
@@ -12,7 +12,6 @@ module.exports = {
         .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
         .use('svgo-loader').loader('svgo-loader')
         .tap(options=>({...options,plugins:[{removeAttrs:{attr:'fill'}}]})).end()
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
   }
