eb9bc2cd7d9ff4aa8d2c9bea175bcb7030c60fd8
bbter <1035675608@qq.com>
2020-10-10 15:11:57 +0800

    收集四个组件的数据到 record

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,18 +2,23 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" v-model="value" placeholder="在这里输入备注">
+      <input type="text" v-model="text" placeholder="在这里输入备注">
     </label>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Notes extends Vue {
-  value = '';
+  @Prop() readonly value!: number;
+  text = this.value;
+  @Watch('text')
+  onValueChange(value: string) {
+    this.$emit('update:value', value);
+  }
 }
 </script>
 
