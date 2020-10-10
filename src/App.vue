546fa2ec406be15b131fd0bcb918d76244e8484e
bbter <1035675608@qq.com>
2020-10-10 19:45:31 +0800

    完成Labels.vue HTML CSS

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,6 @@ body{
   font-family: $font-hei;
   line-height: 1.5;
   font-size: 16px;
+  background: #f5f5f5;
 }
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,19 @@
 <template>
-    <Layout>
-      Labels.vue
-    </Layout>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -9,3 +21,38 @@ export default {
   name: 'Labels',
 };
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+
+    svg {
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper{
+    text-align: center;
+    padding: 16px;
+    margin-top: 44 - 16px;
+  }
+}
+</style>
