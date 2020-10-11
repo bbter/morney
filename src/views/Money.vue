dc3394d56b90a34ea1488eb2aa82e14ca489ce65
bbter <1035675608@qq.com>
2020-10-11 20:39:25 +0800

    修复store的bug 值与地址

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,7 +11,7 @@
 <script lang="ts">
 
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue'
+import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
@@ -19,14 +19,19 @@ import {Component} from 'vue-property-decorator';
 import store from '@/store/index2';
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad}
+  components: {Tags, FormItem, Types, NumberPad},
+  computed: {
+    recordList() {
+      return store.recordList;
+    }
+  }
 })
 export default class Money extends Vue {
-  recordList = store.recordList;
+
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   saveRecord() {
-    store.createRecord(this.record)
+    store.createRecord(this.record);
   }
 
 
@@ -37,7 +42,8 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
+
+.notes {
   padding: 12px 0;
 }
 </style>
