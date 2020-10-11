4a073ed9a9da0f1e46ada8c4d1a4a9c2452344ba
bbter <1035675608@qq.com>
2020-10-11 17:40:27 +0800

    封装record的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -11,21 +11,19 @@
 <script lang="ts">
 
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
+import Types from '@/components/Money/Types.vue'
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/model/recordListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList = recordList;
+  recordList = window.recordList;
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -33,13 +31,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record)
+    window.createRecord(this.record)
   }
 
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
 
 }
 </script>
