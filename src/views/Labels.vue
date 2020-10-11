50d3ba751204837dfdf6e75ee3ac3d2289d0bee0
bbter <1035675608@qq.com>
2020-10-11 16:17:18 +0800

    封装window.createTag

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,6 +18,7 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/model/tagListModel';
 import Button from '@/components/Money/Button.vue';
 
+
 @Component({
   components: {Button}
 })
@@ -27,12 +28,7 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
     if (name) {
-      const message = tagListModel.create(name);
-      if (message === 'duplicated') {
-        window.alert('标签名重复');
-      } else if (message === 'success') {
-        window.alert('添加成功');
-      }
+      window.createTag(name)
     }
   }
 }
