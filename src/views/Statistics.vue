690ebc0f8ad8b1071a97f094a516d1a3ff5144a2
bbter <1035675608@qq.com>
2020-10-12 18:41:47 +0800

    完成Statistics.vue CSS部分

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,11 +2,18 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type: {{ type }}
-      <br>
-      interval:{{ interval }}
-    </div>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li class="record" v-for="item in group.items" :key="item.id">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
@@ -21,6 +28,30 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = { title: string; items: RecordItem[] }
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
@@ -30,20 +61,44 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
-::v-deep .type-tabs-item {
-  background: white;
+::v-deep {
+  .type-tabs-item {
+    background: white;
 
-  &.selected {
-    background: #c4c4c4;
+    &.selected {
+      background: #c4c4c4;
 
-    &::after {
-      display: none;
+      &::after {
+        display: none;
+      }
     }
   }
-}
 
-::v-deep .interval-tabs-item {
-  height: 48px;
-}
+  .interval-tabs-item {
+    height: 48px;
+  }
 
+
+  %item {
+    padding: 0 16px;
+    min-height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .title {
+    @extend %item
+  }
+
+  .record {
+    background: white;
+    @extend %item
+  }
+  .notes{
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999999;
+  }
+}
 </style>
