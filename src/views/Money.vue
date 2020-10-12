ed6a8ff8ade555d8f42f1839208562076abc0404
bbter <1035675608@qq.com>
2020-10-12 16:13:52 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -12,7 +12,6 @@
 <script lang="ts">
 
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
@@ -21,7 +20,7 @@ import recordTypeList from '@/constants/recordTypeList';
 import Tabs from '@/components/Tabs.vue';
 
 @Component({
-  components: {Tabs, Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem,  NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {
